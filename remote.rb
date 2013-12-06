8907288eec9bdaa085a77261e0f8e48b28129a9d
Capybara <git.capybara@gmail.com>
2013-12-06 00:10:18 -0500

    still refactoring, added to help menu and made more consistent between modes

--- remote.rb
+++ remote.rb
@@ -4,57 +4,50 @@
 # ruby 2.0.0p247
 require 'net/telnet'
 require 'open-uri'
+module Pressable
+   def keypress key
+     begin
+       if self.class == Plex
+         open("http://10.0.1.23:32400/system/players/10.0.1.23/navigation/#{@cmds[key]}")
+       else
+         con = Net::Telnet::new('Host' => @host, 'Port' => @port, 'Wait-time' => 0.1, 'Prompt' => /.*/, 'Telnet-mode' => false)
+         con.cmd("#{@prefix + @cmds[key]}")
+         con.close
+       end
+     rescue
+       puts "#{key} is not a valid command, press 'H' for help"
+     end
+   end
+end
 # Plex Home Theatre, I think this uses a new port now. Plex-Ruby gem wouldn't work for me
 class Plex
-  CMDS = { 'k' => 'moveUp', 'j' => 'moveDown', 'h' => 'moveLeft', 'l' => 'moveRight', 'K' => 'pageUp', 'J' => 'pageDown', ' ' => 'select', 'b' => 'back', 't' => 'toggleOSD' }
-  def keypress key
-    begin
-      open("http://10.0.1.23:32400/system/players/10.0.1.23/navigation/#{CMDS[key]}")
-    rescue
-      puts "can't connect to Plex" #would like something more helpful
-    end
+  attr_reader :cmds
+  include Pressable
+  def initialize 
+    @cmds = { 'k' => 'moveUp', 'j' => 'moveDown', 'h' => 'moveLeft', 'l' => 'moveRight', 'K' => 'pageUp', 'J' => 'pageDown', ' ' => 'select', 'b' => 'back', 't' => 'toggleOSD' }
   end
 end
 
 # Tivo Series 3
 class Tivo
-  CMDS = { 'j' => 'CHANNELDOWN', 'k' => 'CHANNELUP', 'J' => 'DOWN', 'K' => 'UP','l' => 'RIGHT', 'h' => 'LEFT', 'g' => 'GUIDE', ' ' => 'SELECT', 'T' => 'THUMBSUP', 't' => 'THUMBSDOWN', 'r' => 'RECORD' }
-  def initialize 
-    host="10.0.1.5"
-    port=31339
-    @tiv = Net::Telnet::new('Host' => host, 'Port' => port, 'Wait-time' => 0.1, 'Prompt' => /.*/, 'Telnet-mode' => false)
-  end
-  def keypress key
-    begin
-      @tiv.cmd("IRCODE #{CMDS[key]}")
-      @tiv.close
-    rescue
-      @tiv = Net::Telnet::new('Host' => '10.0.1.5', 'Port' => 31339, 'Wait-time' => 0.1, 'Prompt' => /.*/, 'Telnet-mode' => false)
-      @tiv.cmd("IRCODE #{CMDS[key]}")
-      @tiv.close
-    end
+  attr_reader :cmds
+  include Pressable
+  def initialize
+    @host, @port, @prefix = "10.0.1.5", 31339, "IRCODE "
+    @cmds = { 'j' => 'CHANNELDOWN', 'k' => 'CHANNELUP', 'J' => 'DOWN', 'K' => 'UP','l' => 'RIGHT', 'h' => 'LEFT', 'g' => 'GUIDE', ' ' => 'SELECT', 'T' => 'THUMBSUP', 't' => 'THUMBSDOWN', 'r' => 'RECORD' }
   end
 end
 
 # Yamaha RX-V473, probably works with several models
 class Yamaha
-  CMDS = { 'k' => 'VOL=Up 5 dB', 'j' => 'VOL=Down 5 dB','p' => "PWR=On", 'P' => "PWR=Off", 'h' => "INP=HDMI1", 'l' => "INP=HDMI2" }
-  def initialize host="10.0.1.9", port=50000
-    @yam = Net::Telnet::new('Host' => host, 'Port' => port, 'Wait-time' => 0.1, 'Prompt' => /.*/, 'Telnet-mode' => false)
-  end
-  def keypress key
-    begin
-      @yam.cmd("@MAIN:#{CMDS[key]}")
-      @yam.close
-    rescue
-      @yam = Net::Telnet::new('Host' => host, 'Port' => port, 'Wait-time' => 0.1, 'Prompt' => /.*/, 'Telnet-mode' => false)
-      @yam.cmd("@MAIN:#{CMDS[key]}")
-      @yam.close
-    end
+  attr_reader :cmds
+  include Pressable
+  def initialize
+    @host, @port, @prefix = "10.0.1.9", 50000, "@MAIN:"
+    @cmds = { 'k' => 'VOL=Up 5 dB', 'j' => 'VOL=Down 5 dB','p' => "PWR=On", 'P' => "PWR=Off", 'h' => "INP=HDMI1", 'l' => "INP=HDMI2" }
   end
 end
 
-#class Remote
 @status = 'go'
 @mode = Tivo
 until @status == 'q'
@@ -93,7 +86,9 @@ until @status == 'q'
   when 'H'
     puts "#{@mode} commands"
     puts "Key : Command"
-    @mode::CMDS.each { |key,value| puts " #{key}  : #{value}" }
+    puts " H  : help"
+    puts " q  : quit"
+    @mode.new.cmds.each { |key,value| puts " #{key}  : #{value}" }
   else
     @mode.new.keypress(str)
   end
